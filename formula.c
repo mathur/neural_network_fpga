19377b5ef4bbec7bba86fd9d35e4ff0f004a805a
Rohan Mathur <rohanmathur34@gmail.com>
2016-11-26 15:52:33 -0500

    Remove unused functions
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- formula.c
+++ formula.c
@@ -2,24 +2,14 @@
 
 double sigmoid(double x) {
     if(!ON_FPGA) {
-        // return 1. / (1. + exp(-x));
-    	// return (atan(x) + 1) / 2;
-        // return (0.5) * ( ( x / (1 + fabs(x)) ) + 1);
-        // return (0.5) * ( cbrt( x / (1 + fabs(x)) ) + 1);
         int sign = x > 0;
         x = fabs(x);
 
         double pow0 = 1;
     	double pow1 = x;
-    	double pow2 = pow1*x;
-    	double pow3 = pow2*x;
-    	double pow4 = pow3*x;
+    	double pow2 = pow1 * x;
 
-        double approx = pow0 +
-        				pow1 +
-        				pow2/2.;/* +
-                        pow3/6. +
-                        pow4/24.;*/
+        double approx = pow0 + pow1 + pow2/2.0;
 
         if (sign) {
         	return approx / (1.0 + approx);
@@ -57,20 +47,4 @@ double inv_err(double o, double t) {
 		// nothing yet
 		return 0.0;
 	}
-}
-
-/* Schraudolph's published algorithm */
-double exp_fast_schraudolph(double a) {
-  union { double d; int x[2]; } u;
-  u.x[1] = (int) (1512775 * a + 1072632447);
-  u.x[0] = 0;
-  return u.d;
-}
-
-double factorial(double n) {
-	double factorial = 1;
-	for(int i = 1; i <= n; ++i) {
-		factorial *= i;              // factorial = factorial*i;
-	}
-    return factorial;
 }
